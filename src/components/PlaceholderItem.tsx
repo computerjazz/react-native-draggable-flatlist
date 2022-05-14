cb2fbac703e5edc3f8c6edd1e46375f058797c53
computerjazz <daniel.r.merrill@gmail.com>
2022-05-14 15:45:56 -0700

    bugfix placeholder

--- src/components/PlaceholderItem.tsx
+++ src/components/PlaceholderItem.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { StyleSheet } from "react-native";
-import Animated, { useAnimatedStyle } from "react-native-reanimated";
+import Animated, {
+  runOnJS,
+  useAnimatedReaction,
+  useAnimatedStyle,
+} from "react-native-reanimated";
 import { useAnimatedValues } from "../context/animatedValueContext";
 import { useDraggableFlatListContext } from "../context/draggableFlatListContext";
 import { useRefs } from "../context/refContext";
@@ -12,16 +16,27 @@ type Props<T> = {
 };
 
 function PlaceholderItem<T>({ renderPlaceholder }: Props<T>) {
+  const [size, setSize] = useState(0);
   const {
     activeCellSize,
     placeholderOffset,
     spacerIndexAnim,
     horizontalAnim,
   } = useAnimatedValues();
-
   const { keyToIndexRef, propsRef } = useRefs<T>();
+  const { activeKey, horizontal } = useDraggableFlatListContext();
 
-  const { activeKey } = useDraggableFlatListContext();
+  // Size does not seem to be respected when it is an animated style
+  useAnimatedReaction(
+    () => {
+      return activeCellSize.value;
+    },
+    (cur, prev) => {
+      if (cur !== prev) {
+        runOnJS(setSize)(cur);
+      }
+    }
+  );
 
   const activeIndex = activeKey
     ? keyToIndexRef.current.get(activeKey)
@@ -38,19 +53,16 @@ function PlaceholderItem<T>({ renderPlaceholder }: Props<T>) {
           : { translateY: placeholderOffset.value },
       ],
     };
-    if (horizontalAnim.value) {
-      style.width = activeCellSize.value;
-    } else {
-      style.height = activeCellSize.value;
-    }
 
     return style;
   }, [spacerIndexAnim, placeholderOffset, horizontalAnim]);
 
+  const extraStyle = horizontal ? { width: size } : { height: size };
+
   return (
     <Animated.View
       pointerEvents={activeKey ? "auto" : "none"}
-      style={[StyleSheet.absoluteFill, animStyle]}
+      style={[StyleSheet.absoluteFill, animStyle, extraStyle]}
     >
       {!activeItem || activeIndex === undefined
         ? null
